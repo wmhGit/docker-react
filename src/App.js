1338e44cddbfd7f5e29796e0977c9aa55a7dd574
Bill Healy <billmhealy@gmail.com>
2019-05-30 15:37:54 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Salem!
+          Hello Salem from feature branch!
         </p>
         <a
           className="App-link"
